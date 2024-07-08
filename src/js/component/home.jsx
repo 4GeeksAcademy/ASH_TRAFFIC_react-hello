c687745c0b91305edfac0b25b1afede98e863adb
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-08 21:34:09 +0000

    2da subida arreglo semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,10 +12,14 @@ const Home = () => {
 	}
 
 	return (
-		<div className="d-flex flex-column bg-dark justify-content-center">
-			<div onClick={changeColor} id="red" className={`mx-auto rounded-pill bg-danger ${color == "red" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px' }}></div>
-			<div onClick={changeColor} id="yellow" className={`mx-auto rounded-pill bg-warning ${color == "yellow" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px' }}></div>
-			<div onClick={changeColor} id="green" className={`mx-auto rounded-pill bg-success ${color == "green" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px' }}></div>
+		<div className="d-flex flex-column align-items-center justify-content-center" style={{ height: '100vh' }}>
+			<div className="d-flex flex-column align-items-center bg-dark p-3 rounded">
+				<div className="bg-light p-2 rounded">
+					<div onClick={changeColor} id="red" className={`rounded-pill bg-danger ${color == "red" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px', marginBottom: '10px' }}></div>
+					<div onClick={changeColor} id="yellow" className={`rounded-pill bg-warning ${color == "yellow" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px', marginBottom: '10px' }}></div>
+					<div onClick={changeColor} id="green" className={`rounded-pill bg-success ${color == "green" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px' }}></div>
+				</div>
+			</div>
 		</div>
 	);
 };
