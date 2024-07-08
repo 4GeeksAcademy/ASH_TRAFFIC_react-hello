467cb5e48711a7e1dc9b90f4b6bbf2405175bce6
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-07-08 15:41:40 +0000

    subida uno

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+
+	let [ color, setColor ] = useState("");
+
+	function changeColor(e) {
+		setColor(e.target.id);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column bg-dark justify-content-center">
+			<div onClick={changeColor} id="red" className={`mx-auto rounded-pill bg-danger ${color == "red" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px' }}></div>
+			<div onClick={changeColor} id="yellow" className={`mx-auto rounded-pill bg-warning ${color == "yellow" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px' }}></div>
+			<div onClick={changeColor} id="green" className={`mx-auto rounded-pill bg-success ${color == "green" ? "opacity-100" : "opacity-50"}`} style={{ height: '50px', width: '50px' }}></div>
 		</div>
 	);
 };
